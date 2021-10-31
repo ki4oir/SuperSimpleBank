83aa37de6cd771b568437630336a52afe223795d
Curtis Morris <ki4oir@users.noreply.github.com>
2021-10-31 00:21:25 -0400

    Update RunMe.bat

--- RunMe.bat
+++ RunMe.bat
@@ -3,3 +3,7 @@ cd src
 java account.AccountManager
 echo on
 rem pause
+rem To compile this source code on the command line you must be in the package directory.
+rem C:\users\program\src\package> 
+rem Type javac *.java
+rem Return to the src folder to run java package.ClassMain
